284ed3714aceab8527a0216dc3e655f643f4f9a2
Manoj G <manojggm14@gmail.com>
2025-06-13 13:24:45 +0530

    Fix ALLOWED_HOSTS and static setup for Render

--- hotelbookingsystem/settings.py
+++ hotelbookingsystem/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e+985ovpff45@kpg(hj(pphvmxts--*!@hsj+(d1lm(l=9cxqf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://hotelbookingsystem-5qvj.onrender.com']
 
 
 # Application definition
