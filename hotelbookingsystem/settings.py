b1d8994c144337cd9bb3cfbb200adf9664298db1
Manoj G <manojggm14@gmail.com>
2025-06-13 13:08:47 +0530

    Prepared for Render deployment

--- hotelbookingsystem/settings.py
+++ hotelbookingsystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e+985ovpff45@kpg(hj(pphvmxts--*!@hsj+(d1lm(l=9cxqf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
