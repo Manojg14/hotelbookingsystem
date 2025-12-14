29a65b0c60b6a92fb3c12c0201840d2cc3950d05
Manoj G <manojggm14@gmail.com>
2025-12-14 18:06:24 +0530

    commit all
    commit

--- hotelbookingsystem/build.sh
+++ hotelbookingsystem/build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Apply database migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
